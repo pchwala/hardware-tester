b2f11882f3924d31b39eae8b14efb9bf46566312
pchwala.vedion <przemyslaw.chwala6@gmail.com>
2025-03-21 13:17:02 +0100

    Changes to make sudo components launch properly on a new updated linux

--- git-pull.sh
+++ git-pull.sh
@@ -3,5 +3,5 @@ git status
 git switch feature
 git pull
 cp displays.xml /home/serwis/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
-./hardware-tester.sh
+sudo ./hardware-tester.sh
 read -n1 -r -p "Press any key..."
