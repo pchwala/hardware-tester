59191c36d634127b9982b54d8057add2541fe2c6
Przemysław Chwała <136999708+pchwala@users.noreply.github.com>
2025-11-17 17:30:11 +0100

    Remove cd command from repair script

--- repair.sh
+++ repair.sh
@@ -1,6 +1,5 @@
 sudo chown -R vedion:vedion /home/vedion/hardware-tester
 sudo chmod -R 755 /home/vedion/hardware-tester
-cd ./hardware-tester
 git checkout feature
 git reset --hard origin/feature
 git pull
