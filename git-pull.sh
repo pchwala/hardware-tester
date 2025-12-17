f41c1ebbcd60c8db432c729ed0cd4a06251b14ab
pchwala.vedion <przemyslaw.chwala6@gmail.com>
2025-12-17 13:48:19 +0100

    Refactor git-pull.sh to ensure executable permissions are set for all shell scripts

--- git-pull.sh
+++ git-pull.sh
@@ -2,5 +2,13 @@
 source .venv/bin/activate
 git checkout feature
 git pull
+
+# Set executable permissions for shell scripts
+chmod +x hardware-tester.sh
+chmod +x tester_gui.sh
+chmod +x git-pull.sh
+chmod +x repair.sh
+chmod +x fix-grub/fix-grub.sh
+
 sudo /home/vedion/hardware-tester/hardware-tester.sh
 /home/vedion/hardware-tester/tester_gui.sh
